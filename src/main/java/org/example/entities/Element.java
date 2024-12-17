8f1cd934dc7fdf2b3ec7ba03f3ee6229cc62f87d
LayGomez <laylamelanygomez@gmail.com>
2024-12-17 13:09:29 +0100

    feat: add entity Group

---
+++ src/main/java/org/example/entities/Element.java
@@ -0,0 +1,50 @@
+package org.example.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Element {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "athomicNumber")
+    private int atomicNumber;
+
+    @Column(name = "symbol")
+    private String symbol;
+
+    @Column(name= "discoveryDate")
+    private LocalDate discoveryDate;
+
+    @Column(name = "discoveredBy")
+    private String discoveredBy;
+
+    @ManyToOne
+    @JoinColumn(name = "group")
+    private Group group;
+
+
+    public Element(String name, int atomicNumber, String symbol, String discoveredBy, LocalDate discoveryDate, Group group) {
+        this.name = name;
+        this.atomicNumber = atomicNumber;
+        this.symbol = symbol;
+        this.discoveredBy = discoveredBy;
+        this.discoveryDate = discoveryDate;
+        this.group = group;
+    }
+}
--- src/main/java/org/example/entities/Group.java
+++ src/main/java/org/example/entities/Group.java
@@ -1,35 +1,36 @@
 package org.example.entities;
 
+
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+import java.time.LocalDate;
 import java.util.List;
+
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 
-public class elements {
+public class Group {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(nullable = false)
     private Long id;
 
-    @Column(name = "name")
+    @Column
     private String name;
 
-    @Column(name = "athomicNumber")
-    private int atomicNumber;
+    @Column
+    private  String description;
 
-    @Column(name = "symbol")
-    private String symbol;
+    @Column
+    private LocalDate defined_date;
 
-
-
-
+    @OneToMany(mappedBy = "group", cascade = CascadeType.ALL)
+    private List<Element> listElement;
 }
